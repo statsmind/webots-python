73d6cf817baad047f2141d269686f792952ac21f
cnjameshu <jameshu@live.com>
2024-11-09 03:20:13 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+  name="webots",
+  version="1.0.0",
+  author="James Hu",
+  author_email="jameshu@live.com",
+  description="Webots API Wrapper R2023b",
+  long_description="Webots API Wrapper",
+  long_description_content_type="text/markdown",
+  url="https://github.com/statsmind/webots-python",
+  packages=setuptools.find_packages(),
+  classifiers=[
+  "Programming Language :: Python :: 3",
+  "License :: OSI Approved :: MIT License",
+  "Operating System :: OS Independent",
+  ]
+)
